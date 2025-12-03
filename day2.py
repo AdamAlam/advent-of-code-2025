d4ef2efd3b2f54d190bbb28217c503130d1ede07
Adam <adamalam7@outlook.com>
2025-12-02 22:54:03 -0600

    Completed day 2

--- day2.py
+++ day2.py
@@ -2,7 +2,7 @@ f = open("./day2_input.txt", "r")
 ids = f.readline().strip().split(",")
 f.close()
 
-running = 0
+running_pt_1 = 0
 
 for id in ids:
     start, finish = id.split("-")
@@ -13,6 +13,28 @@ for id in ids:
         if num_len % 2 == 1:
             continue
         if num_str[:num_len//2] == num_str[(num_len//2):]:
-            running += num
+            running_pt_1 += num
+            
+def has_repeating_sequence(n):
+    s = str(n)
+    length = len(s)
+    
+    for pattern_len in range(1, length // 2 + 1):
+        if length % pattern_len == 0:
+            pattern = s[:pattern_len]
+            if pattern * (length // pattern_len) == s:
+                return True
+    
+    return False
 
-print(running)
+
+running_pt_2 = 0
+for id in ids:
+    start, finish = id.split("-")
+    
+    for num in range(int(start), int(finish) + 1):
+        if has_repeating_sequence(num):
+            running_pt_2 += num
+    
+
+print(running_pt_2)
