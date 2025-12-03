505435954a066307550da4ec2ca992957a9ac5f1
Adam <adamalam7@outlook.com>
2025-12-03 03:09:12 -0600

    completed day 3

--- day3.py
+++ day3.py
@@ -2,23 +2,34 @@ f = open("./day3_input.txt", "r")
 lines = f.readlines()
 f.close()
 running = 0
+
 for line in lines:
     nums = list(line.strip())
-    cur_max_idx = 0
-    for i, num in enumerate(nums):
-        if i == len(nums) - 1:
-            break
-        if int(num) > int(nums[cur_max_idx]):
-            cur_max_idx = i
     
-    sec_max_idx = cur_max_idx + 1
-    for j in range(cur_max_idx + 1, len(nums)):
-        if int(nums[j]) > int(nums[sec_max_idx]):
-            sec_max_idx = j
+    if len(nums) < 12:
+        continue
     
-    running += int(nums[cur_max_idx] + nums[sec_max_idx])
+    # Using a greedy approach: at each position, pick the largest digit
+    # which still leaves enough digits remaining
+    selected_indices = []
+    start_pos = 0
+    
+    for digit_position in range(12):
+        remaining_needed = 12 - digit_position - 1
+        latest_pos = len(nums) - remaining_needed - 1
+        
+        max_digit = nums[start_pos]
+        max_idx = start_pos
+        
+        for i in range(start_pos, latest_pos + 1):
+            if nums[i] > max_digit:
+                max_digit = nums[i]
+                max_idx = i
+        
+        selected_indices.append(max_idx)
+        start_pos = max_idx + 1
+    
+    result = ''.join(nums[i] for i in selected_indices)
+    running += int(result)
 
 print(running)
-        
-    
-    
