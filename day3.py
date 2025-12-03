b448ca4cf98b6edd1c272376c41aeb086613a52d
Adam <adamalam7@outlook.com>
2025-12-03 03:06:31 -0600

    Completed day 3 pt 1

--- day3.py
+++ day3.py
@@ -1,6 +1,24 @@
 f = open("./day3_input.txt", "r")
 lines = f.readlines()
 f.close()
-
+running = 0
 for line in lines:
-    print(line.strip())
+    nums = list(line.strip())
+    cur_max_idx = 0
+    for i, num in enumerate(nums):
+        if i == len(nums) - 1:
+            break
+        if int(num) > int(nums[cur_max_idx]):
+            cur_max_idx = i
+    
+    sec_max_idx = cur_max_idx + 1
+    for j in range(cur_max_idx + 1, len(nums)):
+        if int(nums[j]) > int(nums[sec_max_idx]):
+            sec_max_idx = j
+    
+    running += int(nums[cur_max_idx] + nums[sec_max_idx])
+
+print(running)
+        
+    
+    
